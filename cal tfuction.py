b9aca4ff45e569cb914c4644d253eb3d817e2433
kaushik <krsit22@gmail.com>
2025-12-30 15:42:37 +0530

    Add files via upload

---
+++ cal tfuction.py
@@ -0,0 +1,27 @@
+
+
+def sum(a,b): 
+    gr = a+b
+    return gr 
+
+def sub(a,b): 
+    gr = a-b
+    return gr 
+
+def  mul(a,b): 
+    gr = a*b
+    return gr 
+    
+def div(a,b): 
+    gr = a/b
+    return gr 
+
+a = sum(3,5) 
+b=sub(6,4)
+c=mul(2,4)
+d=div(4,2)
+
+print ("addition is", a)
+print ("substration is",b)
+print ("multiplication is",c)
+print ("division is",d)
